863210f9cb5235d9ec6f1446d9c279f7dd507497
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2026-01-07 17:12:56 -0400

    adicionando hooks e tirando a logica de sellingPage

---
+++ front-end/src/features/selling/hooks/useSelling.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import { ProductType } from '../types/products';
+import { getAllProducts } from '../../../services/productsService';
+
+export function useSelling() {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [searchResults, setSearchResults] = useState<ProductType[]>([]);
+  const [selectedProductIds, setSelectedProductsIds] = useState<number[]>([]);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const [searchbarInput, setSearchbarInput] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const productsList = await getAllProducts();
+      setProducts(productsList);
+    };
+
+    fetchData();
+  }, []);
+
+  const availableProducts = searchResults.filter(
+    product => !selectedProductIds.includes(product.id),
+  );
+
+  const selectedProducts = products.filter(product =>
+    selectedProductIds.includes(product.id),
+  );
+
+  const handleSearch = (value: string) => {
+    if (!value.trim()) {
+      setSearchResults([]);
+      return;
+    }
+
+    const result = products.filter(product =>
+      product.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()),
+    );
+
+    setSearchResults(result);
+  };
+
+  const onEnter = (selectProductIndex: number) => {
+    if (availableProducts.length > 0) {
+      const firstProduct = availableProducts[selectProductIndex];
+      setSelectedProductsIds(prev => [... prev, firstProduct.id]);
+      setSearchResults([]);
+    }
+  };
+
+  const clickOnProduct = (product: ProductType) => {
+    setSelectedProductsIds(prev => [...prev, product.id]);
+    setSearchbarInput('');
+    handleSearch('');
+  };
+
+  return {
+    availableProducts,
+    selectedProducts,
+    highlightedIndex,
+    searchbarInput,
+
+    setHighlightedIndex,
+    setSearchbarInput,
+
+    handleSearch,
+    onEnter,
+    clickOnProduct,
+  };
+}
--- front-end/src/pages/SellingPage.tsx
+++ front-end/src/pages/SellingPage.tsx
@@ -1,59 +1,20 @@
-import { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { getAllProducts } from '../services/productsService';
-import { ProductType } from '../features/selling/types/products';
-import { SearchBar, ProductsDisplay, ShoppingCart } from '../features/selling';
+import { SearchBar, ProductsDisplay, ShoppingCart, useSelling } from '../features/selling';
 
 function SellingPage() {
-  const [products, setProducts] = useState<ProductType[]>([]);
-  const [searchResults, setSearchResults] = useState<ProductType[]>([]);
-  const [selectedProductIds, setSelectedProductsIds] = useState<number[]>([]);
-  const [highlightedIndex, setHighlightedIndex] = useState(0);
-  const [searchbarInput, setSearchbarInput] = useState('');
+  const {
+    availableProducts,
+    selectedProducts,
+    highlightedIndex,
+    searchbarInput,
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const productsList = await getAllProducts();
-      setProducts(productsList);
-    };
+    setHighlightedIndex,
+    setSearchbarInput,
 
-    fetchData();
-  }, []);
-
-  const availableProducts = searchResults.filter(
-    product => !selectedProductIds.includes(product.id),
-  );
-
-  const selectedProducts = products.filter(product =>
-    selectedProductIds.includes(product.id),
-  );
-
-  const handleSearch = (value: string) => {
-    if (!value.trim()) {
-      setSearchResults([]);
-      return;
-    }
-
-    const result = products.filter(product =>
-      product.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()),
-    );
-
-    setSearchResults(result);
-  };
-
-  const onEnter = (selectProductIndex: number) => {
-    if (availableProducts.length > 0) {
-      const firstProduct = availableProducts[selectProductIndex];
-      setSelectedProductsIds(prev => [... prev, firstProduct.id]);
-      setSearchResults([]);
-    }
-  };
-
-  const selectProduct = (product: ProductType) => {
-    setSelectedProductsIds(prev => [...prev, product.id]);
-    setSearchbarInput('');
-    handleSearch('');
-  };
+    handleSearch,
+    onEnter,
+    clickOnProduct,
+  } = useSelling();
 
   return (
     <Container className="mt-2">
@@ -66,15 +27,15 @@ function SellingPage() {
           resultsCount={ availableProducts.length }
           setInput={ setSearchbarInput }
         />
+
         <Col className="mt-1">
           <ProductsDisplay
             products={ availableProducts }
             highlightedIndex={ highlightedIndex }
-            onSelect={ (product: ProductType) =>
-              selectProduct(product)
-            }
+            onSelect={ clickOnProduct }
           />
         </Col>
+
         <ShoppingCart selectedProducts={ selectedProducts } />
       </Row>
     </Container>
