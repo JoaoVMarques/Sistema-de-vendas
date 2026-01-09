4f084da5b1d78959b8ef69139dfc3097c7f02dd6
JoaoVMarques <j.v.marques.fernandes@gmail.com>
2026-01-09 14:15:47 -0400

    Melhorando o header do site

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,14 +1,43 @@
 import { Container, Row } from 'react-bootstrap';
 import '../styles/styles.css';
-import { House, Cart, CartFill } from 'react-bootstrap-icons';
+import { House,
+  Cart,
+  CartFill,
+  Icon,
+  HouseFill,
+  Briefcase,
+  BriefcaseFill } from 'react-bootstrap-icons';
 
 interface productsDisplayProps {
   SelectedPage: string
 }
 
 export function Header({ SelectedPage }: productsDisplayProps) {
+  const headerSectionFunction = (
+    headerText: string,
+    sectionName: string,
+    NotSelectedIcon: Icon,
+    SelectedIcon: Icon,
+  ) => {
+    if (SelectedPage === sectionName) {
+      return (
+        <span className="text-info">
+          <SelectedIcon className="ms-2 header-icon text-info" size={ 20 } />
+          { headerText }
+        </span>
+      );
+    }
+
+    return (
+      <span>
+        <NotSelectedIcon className="header-icon" size={ 20 } />
+        { headerText }
+      </span>
+    );
+  };
+
   return (
-    <div className="header">
+    <div className="header overflow-hidden">
       <Container className="ms-3">
         <Row className="mt-4">
           <h4>
@@ -19,19 +48,13 @@ export function Header({ SelectedPage }: productsDisplayProps) {
           <hr className="w-75" />
         </Row>
         <Row className="mt-3">
-          <span><House className="header-icon" size={ 20 } /> Home</span>
+          { headerSectionFunction('Home', 'home', House, HouseFill) }
         </Row>
         <Row className="mt-3">
-
-          { (() => {
-            if (SelectedPage !== 'cart') {
-              return <span><Cart className="header-icon" size={ 20 } /> Carrinho</span>;
-            } else {
-              return <span className="text-info"><CartFill
-                className="header-icon text-info"
-                size={ 20 } /> Carrinho</span>;
-            }
-          })() }
+          { headerSectionFunction('Carrinho', 'cart', Cart, CartFill) }
+        </Row>
+        <Row className="mt-3">
+          { headerSectionFunction('Inventario', 'inventory', Briefcase, BriefcaseFill) }
         </Row>
       </Container>
     </div>
